1dc25186b3d5ae3b87c2b5abf612095c139c6259
Jay <140168334+Qdev1226@users.noreply.github.com>
2023-11-24 08:58:36 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 // import FacebookLoginComponent from './FacebookLoginComponent';
-
+import FacebookLogin from 'react-facebook-login';
 function App() {
+  const responseFacebook = (response) => {
+    console.log(response);
+  }
   return (
     <div>
 9
-       
+<FacebookLogin
+    appId="318221891163640"
+    autoLoad={true}
+    fields="name,email,picture"
+    onClick={componentClicked}
+    callback={responseFacebook} />
 10
         {/* <FacebookLoginComponent /> */}
 11
