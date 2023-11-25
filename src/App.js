af33a96e3922df42a1d8914a5b873187bd97289a
Jay <140168334+Qdev1226@users.noreply.github.com>
2023-11-25 14:35:42 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,37 +3,21 @@ import './App.css';
 // import FacebookLoginComponent from './FacebookLoginComponent';
 import FacebookLogin from 'react-facebook-login';
 function App() {
-  const responseFacebook = (response) => {
-    console.log(response);
-  }
-  return (
-    <div>
-
-<FacebookLogin
-    appId="318221891163640"
-    onSuccess={(res)=>{
-      console.log("login success");
-      console.log(res);
-    }} 
-    onFail = {(res)=>{      
-      console.log("login fail");
-      console.log(res);
+    const responseFacebook = (response) => {
+        console.log(response);
     }
-  }
-  onProfileSuccess={(res)=>{
-    console.log("onProfileSuccess success");
-    console.log(res);
-  }
-}
-    />
+    return (
+        <div>
 
-        {/* <FacebookLoginComponent /> */}
+            <FacebookLogin
+                appId="274043938971436"
+                fields="name,email"
+                callback={responseFacebook}
+              /> {/* <FacebookLoginComponent /> */}
 
- 
+        </div>
 
-    </div>
-
-  );
+    );
 }
 
 export default App;
